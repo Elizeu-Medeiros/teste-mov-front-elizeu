df28713f83f5e1e5649ca12636669c3c58d486cb
Elizeu Medeiros <elizeumedeiros@gmail.com>
2022-10-04 09:37:13 -0300

    Service de carros

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cars } from 'src/app/Cars';
+import { CarsService } from 'src/app/services/cars.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  total: Number = 18;
-  constructor() {
+  total: Number = 0;
+  cars: Cars[] = [];
 
-  }
+  constructor(private carsService: CarsService) { }
 
   ngOnInit(): void {
   }
