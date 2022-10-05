628cca81db52b56f9167aaee8d90a78b1cfd8e45
Elizeu Medeiros <elizeumedeiros@gmail.com>
2022-10-05 09:46:01 -0300

    consumindo api

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cars } from 'src/app/Cars';
 import { CarsService } from 'src/app/services/cars.service';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,40 @@ import { CarsService } from 'src/app/services/cars.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  total: Number = 0;
+  items: any;
   cars: Cars[] = [];
+  allCars: Cars[] = [];
+  baseApiUrl = environment.baseApiUrl;
+
+  searchTerm: string = '';
+  // faSearch = faSearch;
 
   constructor(private carsService: CarsService) { }
 
   ngOnInit(): void {
+    this.carsService.getCars().subscribe((itens) => {
+      const data = itens.data;
+      const items = itens;
+
+      data.map((item: { created_at: string | number | Date; }) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allCars = data;
+      this.cars = data;
+      this.items = itens;
+    });
   }
+
+  // search(e: Event): void {
+  //   const target = e.target as HTMLInputElement;
+  //   const value = target.value;
+
+  //   this.moments = this.allMoments.filter((moment) =>
+  //     moment.title.toLowerCase().includes(value)
+  //   );
+  // }
 
 }
--- src/app/services/cars.service.ts
+++ src/app/services/cars.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Cars } from '../Cars';
+import { Cars } from 'src/app/Cars';
+import { Response } from 'src/app/Response';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,4 +15,27 @@ export class CarsService {
   private apiUrl = `${this.baseApiUrl}api/cars`;
 
   constructor(private http: HttpClient) { }
+
+  getCars(): Observable<Response<Cars[]>> {
+    return this.http.get<Response<Cars[]>>(this.apiUrl);
+  }
+
+  getCar(id: number): Observable<Response<Cars>> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Response<Cars>>(url);
+  }
+
+  createCar(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
+  removeCar(id: number) {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url);
+  }
+
+  updateCar(id: number, formData: FormData): Observable<FormData> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.put<FormData>(url, formData);
+  }
 }
